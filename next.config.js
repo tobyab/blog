bac24b66b81ecf6cad46358b9c5bfbde3a5a8e99
Toby <mrtobez08@gmail.com>
2022-06-03 18:16:53 +0100

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,23 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+import { withContentlayer } from 'next-contentlayer'
 
-module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+
+
+const nextConfig = {
+    reactStrictMode: true,
+  }
+
+  const withMDX = require('@next/mdx')({
+    extension: /\.mdx?$/,
+    options: {
+      remarkPlugins: [],
+      rehypePlugins: [],
+      // If you use `MDXProvider`, uncomment the following line.
+      // providerImportSource: "@mdx-js/react",
+    },
+  })
+
+  module.exports = withContentlayer({
+    // Append the default value with md extensions
+    pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+  })
