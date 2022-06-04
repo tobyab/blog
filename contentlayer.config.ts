8b149555a62eb9c239bac6c380af8092e1ed5b8a
Toby <mrtobez08@gmail.com>
2022-06-04 08:32:35 +0100

    changes made

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -6,14 +6,24 @@ const Post = defineDocumentType(() => ({
   fields: {
     title: {
       type: 'string',
-      description: 'It is I, the ghost of The blog!',
+      description: 'This is an example of a blog title',
       required: true,
     },
     date: {
-      type: 'date',
-      description: 'It is I, the ghost of The blog!',
-      required: true,
-    },
+        type: 'date',
+        description: '26/11/2008',
+        required: true,
+   },
+   author: {
+        type: 'string',
+        description: 'Toby',
+        required: true,
+   },
+   preview: {
+    type: 'string',
+    description: 'This is a description! Desciptions are great.',
+    required: true,
+    }
   },
   computedFields: {
     url: {
