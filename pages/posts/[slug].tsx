27e7f97254e679f0d8713307a8e17252317d6d03
Toby <mrtobez08@gmail.com>
2022-06-04 08:35:32 +0100

    changes made

---
+++ pages/posts/[slug].tsx
@@ -0,0 +1,28 @@
+import { allPosts, Post } from '../../.contentlayer/generated'
+import Head from 'next/head'
+
+export async function getStaticPaths() {
+  const paths: string[] = allPosts.map((post) => post.url);
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+const PostLayout = ({ post }: { post: Post }) => {
+  return (
+    <>
+      <Head>
+        <title>{post.title}</title>
+      </Head>
+      <article className="max-w-xl mx-auto py-8">
+        <div className="text-center mb-8">
+          <h1>{post.title}</h1>
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: post.body.html }} />
+      </article>
+    </>
+  );
+};
+
+export default PostLayout;
